75b23e31157dc6ec60e63d954a253e0c91fa6208
christinaw97 <cwang5@caltech.edu>
2022-07-12 15:09:43 -0700

    update for x-ray setup

--- movestage.py
+++ movestage.py
@@ -4,8 +4,8 @@ import argparse
 def parsing():
     parser = argparse.ArgumentParser()
     parser.add_argument("MR", type=int, help="Move right")
-    parser.add_argument("-P", "--port", type=str, default='/dev/ttyUSB0', help='port')
-
+    parser.add_argument("-P", "--port", type=str, default='/dev/ttyUSB1', help='port')
+    #ttyUSB0 is rotation stage and 1 is linear stage with the setup in 119, as of 20220712
     args = parser.parse_args()
     return args
 
